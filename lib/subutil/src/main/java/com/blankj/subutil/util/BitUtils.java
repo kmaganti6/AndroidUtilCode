7690a4694a0d65257d12caac8a3796fb40e55f98
kulasekharmaganti <maganti65@gmail.com>
2021-04-13 23:38:55 -0400

    SATD: deleted long comments:DONE

--- lib/subutil/src/main/java/com/blankj/subutil/util/BitUtils.java
+++ lib/subutil/src/main/java/com/blankj/subutil/util/BitUtils.java
@@ -28,18 +28,7 @@ public final class BitUtils {
         return (byte) ((source >> pos) & 1);
 
     }
-    //TODO: comment is way too long
-    //FIXME: Delete comments for readabilty
 
-    /**
-     * 将运算数指定位置的值置为指定值<br>
-     * 例: 0000 1011 需要更新为 0000 1111, 即第 2 位的值需要置为 1<br>
-     *
-     * @param source 需要运算的数
-     * @param pos    指定位置 (0<=pos<=7)
-     * @param value  只能取值为 0, 或 1, 所有大于0的值作为1处理, 所有小于0的值作为0处理
-     * @return 运算后的结果数
-     */
     public static byte setBitValue(byte source, int pos, byte value) {
 
         byte mask = (byte) (1 << pos);
